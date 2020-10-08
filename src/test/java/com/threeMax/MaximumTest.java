643e733d164255edd8fb489efa4dddd8220b96ed
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-10-09 01:20:36 +0530

    [Kirti]Added:Find Maximum For More Than 3 Elements

--- src/test/java/com/threeMax/MaximumTest.java
+++ src/test/java/com/threeMax/MaximumTest.java
@@ -5,60 +5,59 @@ import org.junit.Test;
 
 public class MaximumTest {
 	@Test
-	public void GivenMaxNumberAmongInteger_atFirstPosition_ReturnSameNumber() {
-		ThreeMax findMax = new ThreeMax(10, 5, 2);
-		Assert.assertEquals(10, findMax.testMaximum());
+	public void TestCase1_1() {
+		Integer[] array= {2,3,8,1,15};
+		ThreeMax findMax = new ThreeMax(array);
+		Assert.assertEquals(15, findMax.testMaximum());
 	}
 
 	@Test
-	public void GivenMaxNumberAmongInteger_atSecondPosition_ReturnSameNumber() {
-		ThreeMax findMax = new ThreeMax(5, 10, 2);
-		Assert.assertEquals(10, findMax.testMaximum());
+	public void TestCase1_2() {
+		Integer [] array= {10,30,20,60,50};
+		ThreeMax findMax = new ThreeMax(array);
+		Assert.assertEquals(60, findMax.testMaximum());
 	}
 
-	@Test
-	public void GivenMaxNumberAmongInteger_atThirdPosition_ReturnSameNumber() {
-		ThreeMax findMax = new ThreeMax(2, 5, 10);
-		Assert.assertEquals(10, findMax.testMaximum());
-	}
-
+	
+/*
 	@Test
 	public void GivenMaxNumberAmongFloat_atFirstPosition_ReturnSameNumber() {
 		ThreeMax findMax = new ThreeMax(10.0F, 5.0F, 2.0F);
 		Assert.assertEquals((Float) 10.F, findMax.testMaximum());
 	}
-
+*/
 	@Test
-	public void GivenMaxNumberAmongFloat_atSecondPosition_ReturnSameNumber() {
-		ThreeMax findMax = new ThreeMax(5.0F,10.0F, 2.0F);
-		Assert.assertEquals((Float) 10.0F, findMax.testMaximum());
+	public void TestCase2_1() {
+		Float[] array= {2.0F,3.0F,16.3F,9.0F,20.0F};
+		ThreeMax findMax = new ThreeMax(array);
+		Assert.assertEquals((Float) 20.0F, findMax.testMaximum());
 	}
 
 	@Test
-	public void GivenMaxNumberAmongFloat_atThirdPosition_ReturnSameNumber() {
-		ThreeMax findMax = new ThreeMax(2.0F, 5.0F, 10.0F);
-		Assert.assertEquals((Float) 10.0F, findMax.testMaximum());
+	public void TestCase2_2() {
+		Float[] array= {2.0F, 5.0F, 10.0F,10.3F,10.9F,10.91F};
+		ThreeMax findMax = new ThreeMax(array);
+		Assert.assertEquals((Float) 10.91F, findMax.testMaximum());
 	}
 
 	@Test
-	public void GivenMaxNumberAmongString_atFirstPosition_ReturnSameNumber() {
-		ThreeMax findMax = new ThreeMax("abe", "abd", "abc");
-
-		Assert.assertEquals("abe", findMax.testMaximum());
+	public void TestCase3_1() {
+		String[] array= {"kirti","kirt","irt","kir","abc"};
+		ThreeMax findMax = new ThreeMax(array);
+		Assert.assertEquals("kirti", findMax.testMaximum());
 	}
 
 	@Test
-	public void GivenMaxNumberAmongString_atSecondPosition_ReturnSameNumber() {
-		ThreeMax findMax = new ThreeMax("abd", "abe", "abc");
-
-		Assert.assertEquals("abe", findMax.testMaximum());
+	public void TestCase3_2() {
+		String[] array= {"capgemini","mini","capg","pgemini","apple","app"};
+		ThreeMax findMax = new ThreeMax(array);
+		Assert.assertEquals("pgemini", findMax.testMaximum());
 	}
-
+/*
 	@Test
 	public void GivenMaxNumberAmongString_atThirdPosition_ReturnSameNumber() {
 		ThreeMax findMax = new ThreeMax("abc", "abd", "abe");
-
 		Assert.assertEquals("abe", findMax.testMaximum());
 	}
-
+*/
 }
